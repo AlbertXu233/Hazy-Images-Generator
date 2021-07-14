f6d4d890f2562b0038488b05756526dd2f693970
AlbertXu <845995433@qq.com>
2021-07-14 20:46:19 +0800

    v1

---
+++ DepthEstimation.py
@@ -0,0 +1,53 @@
+import os
+
+import cv2
+import torch
+import os
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+parser = ArgumentParser(description='generate depth map with MiDaS')
+parser.add_argument('--model_type',default='l',type=str,help='one of {l,m,s}')
+parser.add_argument('--input_dir',default='data/gt',type=str,help='path of input images')
+parser.add_argument('--output_dir',default='data/depth',type=str,help='path of output depth maps')
+args = parser.parse_args()
+# MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+# MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+# MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+types = {'l':"DPT_Large",'m':"DPT_Hybrid",'s':"MiDaS_small"}
+model_type = types[args.model_type]
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+
+def depthPrediction(imgName):
+    img = cv2.imread(imgName)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    input_batch = transform(img).to(device)
+    with torch.no_grad():
+        prediction = midas(input_batch)
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+    output = prediction.cpu().numpy()
+    return output
+imgDir = args.input_dir
+resDir = args.output_dir
+os.makedirs(resDir,exist_ok=True)
+names = os.listdir(imgDir)
+names.sort(key=lambda x:int(x.split('.')[0]))
+for name in names:
+    imgName = imgDir+name
+    res = depthPrediction(imgName)
+    plt.imsave(resDir+name)
+    print(name)
---
+++ dataset.py
@@ -0,0 +1,77 @@
+import torch.utils.data as data
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg", "JPG"])
+
+class HazyData(data.Dataset):
+    def __init__(self, root,alpha,beta,bias,random=True,extraAugment=True,crop_size=480):
+        """
+        :param root: Root path of RGB images and their depth maps
+        :param alpha: Atmospheric light intensity (0,1]
+        :param beta: Atmospheric diffuse reflection coefficient (0,inf)
+        :param bias: Background retention factor [0,1)
+        :param random: Whether to make actual alpha and beta random from (0,alpha) (0,beta)
+        :param extraAugment: Whether to take randomly crop,flip and rotate
+        :param crop_size: the image size after crop, only valid when extraAugment==True
+        """
+        self.root = root
+        self.alpha = alpha
+        self.beta = beta
+        self.bias = bias
+        self.random = random
+        self.extAug = extraAugment
+        self.crop_size = crop_size
+        self.target_dir = os.path.join(self.root, 'gt')
+        self.depth_dir = os.path.join(self.root, 'depth')
+        self.target_ids = [x for x in sorted(os.listdir(self.target_dir)) if is_image_file(x)]
+        self.target_ids.sort(key=lambda x:int(x.split('.')[0]))
+
+
+    def __len__(self):
+        return len(self.target_ids)
+
+    def __getitem__(self, index):
+        #targetImage
+        name = self.target_ids[index]
+        target_image = plt.imread(os.path.join(self.target_dir, name))
+        target_image = np.asarray(target_image, np.float32)
+        target_image /= 255
+        target_image = np.expand_dims(target_image,0)
+        #depthImage
+        dep = plt.imread(os.path.join(self.depth_dir,name))
+        dep = np.expand_dims(dep,0)
+        if self.extAug:
+            #randomly crop
+            hw = self.crop_size
+            _,h,w = target_image.shape
+            i = np.random.randint(h-hw+1)
+            j = np.random.randint(w-hw+1)
+            target_image = target_image[:,i:i+hw,j:j+hw]
+            dep = dep[:, i:i + hw, j:j + hw]
+            #randomly flip
+            flip_channel,rotate_degree = np.random.randint(3,size=2)
+            if flip_channel:
+                target_image=np.flip(target_image,flip_channel)
+                dep = np.flip(dep,flip_channel)
+            #randomly rotate
+            if rotate_degree:
+                target_image=np.rot90(target_image,rotate_degree,(1,2))
+                dep=np.rot90(dep,rotate_degree,(1,2))
+        #hazyImage
+        if self.random:
+            beta = np.random.randint(self.beta)
+            alpha = np.random.rand(self.alpha)
+        else:
+            beta = self.beta
+            alpha = self.alpha
+        trans = np.zeros_like(dep,np.float32)+self.bias
+        np.exp(-beta / dep, out=trans, where=dep > 0)
+        hazy = (target_image*trans + alpha*(1-trans)).astype(np.float32)
+        return hazy.copy(), target_image.copy(), name
+
+
+
